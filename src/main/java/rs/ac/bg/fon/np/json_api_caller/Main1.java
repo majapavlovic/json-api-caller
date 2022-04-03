ef00ae7743f7cf793ccada1778c5a078ad468725
majapavlovic <maja.pavlovic2703@gmail.com>
2022-04-03 20:19:43 +0200

    gotov projekat

--- src/main/java/rs/ac/bg/fon/np/json_api_caller/Main1.java
+++ src/main/java/rs/ac/bg/fon/np/json_api_caller/Main1.java
@@ -14,24 +14,24 @@ import okhttp3.Response;
 
 public class Main1 {
 
-	public static String ACCESS_KEY = "2e4baadf5c5ae6ba436f53ae5558107f";
-
+//	public static String ACCESS_KEY = "2e4baadf5c5ae6ba436f53ae5558107f";
+	//"LNWceecJQtFNrfFyuT3HGNNIE5zcRebp"
+	public static String ACCESS_KEY = "63afVflVasF26yKzbN7JM2bH4pGLlEJv";
 	public static void main(String[] args) {
 
 		try {
 			OkHttpClient client = new OkHttpClient().newBuilder().build();
 
-			String message = "This restaurant has a lovely atmosphere and the staff is great!";
+			String message = "This restaurant has a lovely atmosphere and the staff is great";
 			MediaType mediaType = MediaType.parse("text/plain");
 			@SuppressWarnings("deprecation")
 			RequestBody body = RequestBody.create(mediaType, message);
 
 			Request request = new Request.Builder().url("https://api.apilayer.com/sentiment/analysis")
-					.addHeader("apikey", "LNWceecJQtFNrfFyuT3HGNNIE5zcRebp").method("POST", body).build();
+					.addHeader("apikey", ACCESS_KEY).method("POST", body).build();
 			Response response = client.newCall(request).execute();
-			// System.out.println(response.body().string());
-
-			Gson gson = new GsonBuilder().create();
+			
+			Gson gson = new GsonBuilder().create(); 
 			MessageSentiment ms = new MessageSentiment();
 			ms = gson.fromJson(response.body().string(), MessageSentiment.class);
 			ms.setMessage(message);
