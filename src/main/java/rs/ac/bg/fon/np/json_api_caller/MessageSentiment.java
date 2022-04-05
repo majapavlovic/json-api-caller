a4ec0549b9d787496842780329c01aeb77a5e857
majapavlovic <maja.pavlovic2703@gmail.com>
2022-04-05 22:35:46 +0200

    Revert "gotov projekat"
    This reverts commit ef00ae7743f7cf793ccada1778c5a078ad468725.

--- src/main/java/rs/ac/bg/fon/np/json_api_caller/MessageSentiment.java
+++ src/main/java/rs/ac/bg/fon/np/json_api_caller/MessageSentiment.java
@@ -4,78 +4,51 @@ public class MessageSentiment {
 
 	String message;
 	String sentiment;
-	int score;
-	double confidence;
-	String language;
-	String content_type;
-
-	public MessageSentiment() {
-	}
-
-	public MessageSentiment(String message, String sentiment, int score, double confidence, String language,
-			String content_type) {
-		super();
-		this.message = message;
-		this.sentiment = sentiment;
-		this.score = score;
-		this.confidence = confidence;
-		this.language = language;
-		this.content_type = content_type;
-	}
-
+    int score;
+    double confidence;
+    String language;
+    String content_type;
 	public String getMessage() {
 		return message;
 	}
-
 	public void setMessage(String message) {
 		this.message = message;
 	}
-
 	public String getSentiment() {
 		return sentiment;
 	}
-
 	public void setSentiment(String sentiment) {
 		this.sentiment = sentiment;
 	}
-
 	public int getScore() {
 		return score;
 	}
-
 	public void setScore(int score) {
 		this.score = score;
 	}
-
 	public double getConfidence() {
 		return confidence;
 	}
-
 	public void setConfidence(double confidence) {
 		this.confidence = confidence;
 	}
-
 	public String getLanguage() {
 		return language;
 	}
-
 	public void setLanguage(String language) {
 		this.language = language;
 	}
-
 	public String getContent_type() {
 		return content_type;
 	}
-
 	public void setContent_type(String content_type) {
 		this.content_type = content_type;
 	}
-
 	@Override
 	public String toString() {
 		return "MessageSentiment \n\t[message=" + message + ", \n\tsentiment=" + sentiment + ", \n\tscore=" + score
-				+ ", \n\tconfidence=" + confidence + ", \n\tlanguage=" + language + ", \n\tcontent_type=" + content_type
-				+ "]";
+				+ ", \n\tconfidence=" + confidence + ", \n\tlanguage=" + language + ", \n\tcontent_type=" + content_type + "]";
 	}
-
+    
+    
 }
